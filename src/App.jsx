68346bc519536fc0e6d4d82f1bf4f1a202744b9e
Ton Nom <tonemail@example.com>
2026-02-27 10:13:23 +0100

    creation app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { BrowserRouter as Router , Router , Route } from "react-router-dom"
-import Insertcategorie from "./components/categories/Insertcategorie"
-import Listscategories from "./components/scategories/Listscategories"
-import Updatescategorie from "./components/scategories/Updatescategorie"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Catégories
+import Insertcategorie from "./components/categories/Insertcategorie";
+import Listscategories from "./components/scategories/Listscategories";
+import Updatescategorie from "./components/scategories/Updatescategorie";
+
+// Articles (à créer si ce n'est pas déjà fait)
+import Insertarticle from "./components/articles/Insertarticle";
+import Listarticles from "./components/articles/Listarticles";
+import Updatearticle from "./components/articles/Updatearticle";
 
 function App() {
-  
-
   return (
     <Router>
+      <Routes>
+        {/* Catégories */}
+        <Route path="/listscategories" element={<Listscategories />} />
+        <Route path="/insertcategorie" element={<Insertcategorie />} />
+        <Route path="/updatescategorie" element={<Updatescategorie />} />
 
-    <Route path="/listscategories" element={<Listscategories/>}/>
-     <Route path="/insertcategorie" element={<Insertcategorie/>}/>
-      <Route path="/updatescategorie" element={<Updatescategorie/>}/>
-  
+        {/* Sous-catégories */}
+        <Route path="/scategorieres" element={<Listscategories />} />
+        <Route path="/insertscategorieres" element={<Insertcategorie />} />
+        <Route path="/updatescategorieres" element={<Updatescategorie />} />
+
+        {/* Articles */}
+        <Route path="/articles" element={<Listarticles />} />
+        <Route path="/insertarticle" element={<Insertarticle />} />
+        <Route path="/updatearticle" element={<Updatearticle />} />
+      </Routes>
     </Router>
-  
-  )
+  );
 }
 
-export default App
+export default App;
